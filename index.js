e488cfea63391bb9c7ca32f115ff4495e713f51e
unnospid <70819261+spidunno@users.noreply.github.com>
2022-02-17 16:32:32 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,102 @@
+/**
+	* Initialize inp. Ex. `let input = InputReader(element=document.body)`
+	* @param {HTMLElement} element
+*/
+export default function InputReader(element=window) {
+		const difference = (a1, a2) => a1.filter(x => !a2.includes(x));
+	window.__internal = {
+		down: [],
+		currentUp: [],
+		currentDown: [],
+		currentMouse: {x: 0, y: 0},
+		mouse: {x: 0, y: 0}
+	};
+	element.addEventListener('keydown', e => {
+		if (e.repeat) {
+			return;
+		}
+		if (!__internal.down.includes(e.key)) {
+		__internal.down.push(e.key);
+		}
+	});
+	element.addEventListener('keyup', e => {
+		if (e.repeat) {
+			return;
+		}
+		if (__internal.down.includes(e.key)) {
+		__internal.down = __internal.down.filter(value => value !== e.key);
+		}
+	});
+	element.addEventListener('mousedown', e => {
+		let mb;
+		switch(e.button) {
+			case 0:
+				mb = 'MouseLeft';
+				break;
+			case 1:
+				mb = 'MouseMiddle';
+				break;
+			case 2:
+				mb = 'MouseRight';
+				break;
+			case 3:
+				mb = 'MouseBack';
+				break;
+			case 4:
+				mb = 'MouseForward';
+				break;
+		}
+		if(!__internal.down.includes(mb)) __internal.down.push(mb);
+	});
+	element.addEventListener('mouseup', e => {
+		let mb;
+		switch(e.button) {
+			case 0:
+				mb = 'MouseLeft';
+				break;
+			case 1:
+				mb = 'MouseMiddle';
+				break;
+			case 2:
+				mb = 'MouseRight';
+				break;
+			case 3:
+				mb = 'MouseBack';
+				break;
+			case 4:
+				mb = 'MouseForward';
+				break;
+		}
+		__internal.down = __internal.down.filter(value => value !== mb);
+	});
+	element.addEventListener('mousemove', e => {
+		__internal.mouse.x = e.offsetX;
+		__internal.mouse.y = e.offsetY;
+	});
+	return {
+		down: function(key) {
+			return __internal.currentDown.includes(key);
+		},
+		up: function(key) {
+			return __internal.currentUp.includes(key);
+		},
+		step: function() {
+			__internal.currentUp = difference(__internal.currentDown, __internal.down);
+			__internal.currentDown = __internal.down;
+			__internal.currentMouse = __internal.mouse;
+		},
+		mouse: function(which='both') {
+			switch(which) {
+				case 'both':
+					return __internal.currentMouse;
+				case 'x':
+					return __internal.currentMouse.x;
+				case 'y':
+					return __internal.currentMouse.y;
+				default:
+					throw `Argument "which" requires one of "x", "y", "both", or none at all, instead recieved "${which}"`;
+					break;
+			}
+		}
+	};
+}
---
+++ readme.md
@@ -0,0 +1,37 @@
+
+# Inp
+## State based input library for the browser.
+---
+### Installation:
+Install either install with npm or use a CDN such as unpkg (eg. `https://unpkg.com/inp/index.js`).
+### Usage:
+```js
+import InputReader from 'path/to/inp.js';
+
+let input = InputReader();
+const frameRate = 1000/60;
+function main() {
+	/* input.down tells you if that button is down in the current frame / step. */
+	if(input.down('a')) console.log('Key "A" pressed!');
+	/* input.up does much the same as input.down but instead tells you if the button was released this frame */
+	if(input.up('MouseLeft')) console.log('Left Mouse Button was released this timestep.');
+	/* use input.mouse to get the position of the mouse this frame */
+	console.log(input.mouse('x'), input.mouse('y'));
+	console.log(input.mouse().x, input.mouse('both').y);
+	/* and input.step at the end of you main loop to step forward. Nothing will work unless you do this. */
+	input.step();
+	setTimeout(main, frameRate);
+}
+```
+### API:
+#### `InputReader.down(key: string): boolean`
+Takes in a string of one of a valid `event.key`, `"MouseLeft"`, `"MouseRight"`, `"MouseMiddle"`, `"MouseBack"`, or `"MouseForward"`. Returns a boolean stating whether that button / key is currently down.
+___
+#### `InputReader.up(key: string): boolean`
+Does the inverse of `InputReader.down`, tells you if a key has been released this timestep, takes same arguments as `InputReader.down`.
+___
+#### `InputReader.mouse(which?: string): number | Object`
+Takes in a string with one of `"x"`, `"y"`, `"both"`X position of the mouse, the Y position of the mouse, or `{x: <number>, y: <number>}`, respectively. If no argument is given it defaults to both.
+___
+#### `InputReader.step(): void`
+ Steps forward once and updates everything, required to be called before any inputs can be read correctly.
