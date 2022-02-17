088cb4857ea27f364657d0991225feaaef89ae62
spidunno <70819261+spidunno@users.noreply.github.com>
2022-02-17 19:10:00 +0000

    Fixed some JSDoc stuff

--- index.d.ts
+++ index.d.ts
@@ -2,34 +2,31 @@ interface coordinate {
 	x: number,
 	y: number
 }
-/**
-	* Get whether a key is down.
-	* @param {string} key - The key or button to check.
-	* @returns {boolean}
-*/
-function down(key: string): boolean;
-/**
-	* Get whether a key has been released this timestep.
-	* @param {string} key - The key or button to check.
-	* @returns {boolean}
-*/
-function up(key: string): boolean;
-/**
-	* Steps forward and updates stuff.
-	* @returns {none}
-*/
-function step(): none;
-/**
-	* Gets the position of the mouse.
-	* @param {string} which Whether to return the x, y, or both coordinates.
-	* @return {number | coordinate}
-*/
-function mouse(which?: string): number | coordinate | none;
+
 interface Reader {
-	down: down,
-	up: up,
-	step: step,
-	mouse: mouse
+	/**
+		* Get whether a key is down.
+		* @param {string} key - The key or button to check.
+		* @returns {boolean}
+	*/
+	down: function down(key: string): boolean,
+	/**
+		* Get whether a key has been released this timestep.
+		* @param {string} key - The key or button to check.
+		* @returns {boolean}
+	*/
+	up: function up(key: string): boolean,
+	/**
+		* Steps forward and updates stuff.
+		* @returns {none}
+	*/
+	step: function step(): none,
+	/**
+		* Gets the position of the mouse.
+		* @param {string} which Whether to return the x, y, or both coordinates.
+		* @return {number | coordinate}
+	*/
+	mouse: function mouse(which?: string): number | coordinate | none
 }
 /**
 	* Create a new Reader
