42c2a8a07bdcb0bc3e0f649cfc2ad7288359f29d
spidunno <70819261+spidunno@users.noreply.github.com>
2022-02-18 15:37:54 +0000

    lol turns out I have absolutely no idea how adding type declarations sometimes

--- index.d.ts
+++ index.d.ts
@@ -10,24 +10,24 @@ export interface Reader {
 		* @param {string} key - The key or button to check.
 		* @returns {boolean}
 	*/
-	down: function(key: string): boolean;
+	down(key: string): boolean;
 	/**
 		* Get whether a key has been released this timestep.
 		* @param {string} key - The key or button to check.
 		* @returns {boolean}
 	*/
-	up: function(key: string): boolean;
+	up(key: string): boolean;
 	/**
 		* Steps forward and updates stuff.
 		* @returns {none}
 	*/
-	step: function(): none;
+	step(): none;
 	/**
 		* Gets the position of the mouse.
 		* @param {string} which Whether to return the x, y, or both coordinates.
 		* @return {number | coordinate}
 	*/
-	mouse: function(which?: string): number | coordinate | none;
+	mouse(which?: string): number | coordinate | none;
 }
 /**
 	* Create a new Reader
